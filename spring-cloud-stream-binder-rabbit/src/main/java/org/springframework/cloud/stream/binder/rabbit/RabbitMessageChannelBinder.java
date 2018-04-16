24a7032b0c95fd6739e2f393844bcd455831604d
Gary Russell <grussell@pivotal.io>
2018-04-16 17:54:53 -0400

    GH-139: Advanced listener container configuration
    Resolves https://github.com/spring-cloud/spring-cloud-stream-binder-rabbit/issues/139
    
    Allow configuration of listener container properties that are not exposed as binding
    or binder properties.

--- spring-cloud-stream-binder-rabbit/src/main/java/org/springframework/cloud/stream/binder/rabbit/RabbitMessageChannelBinder.java
+++ spring-cloud-stream-binder-rabbit/src/main/java/org/springframework/cloud/stream/binder/rabbit/RabbitMessageChannelBinder.java
@@ -52,6 +52,7 @@ import org.springframework.cloud.stream.binder.ExtendedConsumerProperties;
 import org.springframework.cloud.stream.binder.ExtendedProducerProperties;
 import org.springframework.cloud.stream.binder.ExtendedPropertiesBinder;
 import org.springframework.cloud.stream.binder.HeaderMode;
+import org.springframework.cloud.stream.binder.rabbit.config.ListenerContainerCustomizer;
 import org.springframework.cloud.stream.binder.rabbit.properties.RabbitCommonProperties;
 import org.springframework.cloud.stream.binder.rabbit.properties.RabbitConsumerProperties;
 import org.springframework.cloud.stream.binder.rabbit.properties.RabbitExtendedBindingProperties;
@@ -125,6 +126,8 @@ public class RabbitMessageChannelBinder
 
 	private final RabbitProperties rabbitProperties;
 
+	private final ListenerContainerCustomizer containerCustomizer;
+
 	private boolean destroyConnectionFactory;
 
 	private ConnectionFactory connectionFactory;
@@ -143,11 +146,18 @@ public class RabbitMessageChannelBinder
 
 	public RabbitMessageChannelBinder(ConnectionFactory connectionFactory, RabbitProperties rabbitProperties,
 			RabbitExchangeQueueProvisioner provisioningProvider) {
+		this(connectionFactory, rabbitProperties, provisioningProvider, null);
+	}
+
+	public RabbitMessageChannelBinder(ConnectionFactory connectionFactory, RabbitProperties rabbitProperties,
+			RabbitExchangeQueueProvisioner provisioningProvider,
+			ListenerContainerCustomizer containerCustomizer) {
 		super(new String[0], provisioningProvider);
 		Assert.notNull(connectionFactory, "connectionFactory must not be null");
 		Assert.notNull(rabbitProperties, "rabbitProperties must not be null");
 		this.connectionFactory = connectionFactory;
 		this.rabbitProperties = rabbitProperties;
+		this.containerCustomizer = containerCustomizer == null ? (c, q, g) -> { } : containerCustomizer;
 	}
 
 	/**
@@ -380,6 +390,7 @@ public class RabbitMessageChannelBinder
 		else if (getApplicationContext() != null) {
 			listenerContainer.setApplicationEventPublisher(getApplicationContext());
 		}
+		this.containerCustomizer.configure(listenerContainer, consumerDestination.getName(), group);
 		listenerContainer.afterPropertiesSet();
 
 		AmqpInboundChannelAdapter adapter = new AmqpInboundChannelAdapter(listenerContainer);
