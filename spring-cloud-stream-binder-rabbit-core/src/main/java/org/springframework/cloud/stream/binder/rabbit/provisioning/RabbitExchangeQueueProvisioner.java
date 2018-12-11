dc014f0d72f9b96cb1d5199b644f0aca0011d5f9
Gary Russell <grussell@pivotal.io>
2018-12-11 14:16:42 -0500

    Polishing - PR Comments

--- spring-cloud-stream-binder-rabbit-core/src/main/java/org/springframework/cloud/stream/binder/rabbit/provisioning/RabbitExchangeQueueProvisioner.java
+++ spring-cloud-stream-binder-rabbit-core/src/main/java/org/springframework/cloud/stream/binder/rabbit/provisioning/RabbitExchangeQueueProvisioner.java
@@ -148,11 +148,13 @@ public class RabbitExchangeQueueProvisioner implements ApplicationListener<Decla
 	private ConsumerDestination doProvisionConsumerDestination(String name, String group,
 			ExtendedConsumerProperties<RabbitConsumerProperties> properties) {
 		boolean anonymous = !StringUtils.hasText(group);
-		String  baseQueueName = properties.getExtension().isQueueNameGroupOnly()
-				? anonymous ? ANONYMOUS_GROUP_NAME_GENERATOR.generateName()
-							: group
-				: anonymous ? groupedName(name, ANONYMOUS_GROUP_NAME_GENERATOR.generateName())
-							: groupedName(name, group);
+		String  baseQueueName;
+		if (properties.getExtension().isQueueNameGroupOnly()) {
+				baseQueueName =  anonymous ? ANONYMOUS_GROUP_NAME_GENERATOR.generateName() : group;
+		}
+		else {
+				baseQueueName = groupedName(name, anonymous ? ANONYMOUS_GROUP_NAME_GENERATOR.generateName() : group);
+		}
 		if (this.logger.isInfoEnabled()) {
 			this.logger.info("declaring queue for inbound: " + baseQueueName + ", bound to: " + name);
 		}
